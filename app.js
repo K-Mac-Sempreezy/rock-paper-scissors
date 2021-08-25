70463c9648d4a4ff96aac6240d9da85bea2b6d18
Kristian MacDonald <kristian.macsempreezy@gmail.com>
2021-08-25 14:52:44 -0700

    refactor ui, refactor css & js

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 //Global variables
 
 const moves = ['rock', 'paper', 'scissors'];
+const svg = {
+  rock: './SVG/rock.svg',
+  paper: './SVG/paper.svg',
+  scissors: './SVG/scissors.svg',
+  question: './SVG/question-mark.svg',
+};
 const button = document.querySelector('button');
 const tiles = document.querySelectorAll('.tile');
-const comTiles = document.querySelectorAll('.com-tile');
+const comTile = document.getElementById('com-tile');
 const images = document.querySelectorAll('.img');
 const scoreboard = document.querySelector('.scoreboard');
-const scoreList = scoreboard.querySelector('#score-list');
-const tileContainer = document.querySelector('#tile-container');
-const comSelectionTiles = document.querySelectorAll('.com-tile');
+const scoreList = document.getElementById('score-list');
 const youScore = document.querySelector('.player-score');
 const comScore = document.querySelector('.computer-score');
-const computer = document.querySelector('.computer');
-const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const wonGame = 'CONGRATULATIONS! YOU WON THE GAME!';
+const lostGame = 'SORRY, YOU LOST THE GAME.';
+const winButton = 'YOU WIN! PLAY AGAIN?';
+const loseButton = 'COMPUTER WINS! PLAY AGAIN?';
+const tiePoint = 'Tie';
+const winPoint = '';
+const losePoint = '';
+const click = 'click to reset';
+const buttonChoice = 'MAKE YOUR CHOICE';
+
 
 let playerScore = 0;
 let computerScore = 0;
@@ -23,96 +36,15 @@ let isGameOver = false;
 
 //Functions
 
-const keepScore = (player, com) => {
-  playerScore += player;
-  computerScore += com;
-
-  youScore.textContent = playerScore;
-  comScore.textContent = computerScore;
-  button.textContent = 'Choose';
-
-  if (playerScore < 5 && computerScore < 5) {
-    button.addEventListener('click', function () {
-      comTiles.forEach((tile) => (tile.style.boxShadow = 'none'));
-      tiles.forEach((tile) => (tile.style.boxShadow = 'none'));
-      tiles.forEach((tile) => tile.addEventListener('mouseover', addShadow));
-      tiles.forEach((tile) =>
-        tile.addEventListener('mouseleave', removeShadow)
-      );
-      comTiles.forEach((comTile) =>
-        comTile.addEventListener('mouseover', addShadow)
-      );
-      comTiles.forEach((comTile) =>
-        comTile.addEventListener('mouseleave', removeShadow)
-      );
-      isGameOver = false;
-      startGame();
-    });
-  } else if (playerScore >= 5) {
-    button.textContent = 'YOU WIN! PLAY AGAIN?';
-
-    let li = document.createElement('li');
-    li.textContent = 'CONGRATULATIONS! YOU WON THE GAME!';
-    scoreList.prepend(li);
-
-    youScore.textContent = playerScore;
-    comScore.textContent = computerScore;
-    playerScore = 0;
-    computerScore = 0;
-    playerSelection = null;
-    computerSelection = null;
-    choiceMade = false;
-    isGameOver = !isGameOver;
-    button.addEventListener('click', playAgain);
-  } else if (computerScore >= 5) {
-    button.textContent = 'COMPUTER WINS! PLAY AGAIN?';
-
-    let li = document.createElement('li');
-    li.textContent = 'SORRY, YOU LOST THE GAME.';
-    scoreList.prepend(li);
-
-    youScore.textContent = playerScore;
-    comScore.textContent = computerScore;
-    playerScore = 0;
-    computerScore = 0;
-    playerSelection = null;
-    computerSelection = null;
-    choiceMade = false;
-    isGameOver = !isGameOver;
-    button.addEventListener('click', playAgain);
-  }
-};
-
-const playAgain = () => {
-  youScore.textContent = playerScore;
-  comScore.textContent = computerScore;
-
-  comTiles.forEach((tile) => (tile.style.boxShadow = 'none'));
-  tiles.forEach((tile) => (tile.style.boxShadow = 'none'));
-  tiles.forEach((tile) => tile.addEventListener('mouseover', addShadow));
-  tiles.forEach((tile) => tile.addEventListener('mouseleave', removeShadow));
-  comTiles.forEach((comTile) =>
-    comTile.addEventListener('mouseover', addShadow)
-  );
-  comTiles.forEach((comTile) =>
-    comTile.addEventListener('mouseleave', removeShadow)
-  );
-  startGame();
-};
-
 const startGame = () => {
   if (isGameOver) {
     playerScore = 0;
     computerScore = 0;
     scoreList.textContent = '';
   }
-
-  button.textContent = 'MAKE YOUR CHOICE';
-
-  comTiles.forEach((comTile) =>
-    comTile.removeEventListener('mouseover', addShadow)
-  );
-
+  button.textContent = buttonChoice;
+  comTile.removeEventListener('mouseover', addShadow);
+  comTile.setAttribute('src', svg['question'])
   tiles.forEach((tile) =>
     tile.addEventListener('click', function () {
       tiles.forEach((tile) =>
@@ -125,11 +57,9 @@ const startGame = () => {
   images.forEach((img) =>
     img.addEventListener('click', function () {
       tiles.forEach((tile) => tile.removeEventListener('mouseover', addShadow));
-
       if (choiceMade) {
         return;
       }
-
       choiceMade = true;
     })
   );
@@ -139,15 +69,18 @@ const startGame = () => {
   );
 };
 
-const translatePlaySelection = () => {
-  playerSelection = this.id;
+const translatePlaySelection = (e) => {
+  playerSelection = e.target.id;
 
   if (playerSelection === 'player-rock') {
-    playerSelection = 'rock';
+    playerSelection = moves[0];
+    comTile.setAttribute('src', svg[moves[0]])
   } else if (playerSelection === 'player-paper') {
-    playerSelection = 'paper';
+    playerSelection = moves[1];
+    comTile.setAttribute('src', svg[moves[1]])
   } else if (playerSelection === 'player-scissors') {
-    playerSelection = 'scissors';
+    playerSelection = moves[2];
+    comTile.setAttribute('src', svg[moves[2]])
   }
 
   computerPlay(playerSelection);
@@ -159,9 +92,9 @@ const translatePlaySelection = () => {
 const computerPlay = (playerSelection) => {
   const computerSelection = moves[Math.floor(Math.random() * moves.length)];
   let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  let colorString = '20px 20px 0px 2px ' + color;
-  const tileID = document.getElementById(computerSelection);
-  tileID.style.boxShadow = colorString;
+  let colorString = '1px 1px 5px 5px ' + color;
+  comTile.setAttribute('src', svg[computerSelection])
+  comTile.style.boxShadow = colorString;
 
   //add box shadow to computerSelection tile here
   game(playerSelection, computerSelection);
@@ -172,10 +105,7 @@ const game = (playerSelection, computerSelection) => {
   const lose = `You lose: ${computerSelection} beats ${playerSelection}!`;
 
   if (playerSelection === computerSelection) {
-    let li = document.createElement('li');
-    scoreList.prepend(li);
-    li.textContent = 'Tie';
-
+    createScoreBoardComment(tiePoint);
     playerSelection = null;
     computerSelection = null;
     keepScore(0, 0);
@@ -186,19 +116,15 @@ const game = (playerSelection, computerSelection) => {
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
+    createScoreBoardComment(win);
     keepScore(1, 0);
-    let li = document.createElement('li');
-    li.textContent = win;
-    scoreList.prepend(li);
   } else if (
     (playerSelection === 'paper' && computerSelection === 'scissors') ||
     (playerSelection === 'rock' && computerSelection === 'paper') ||
     (playerSelection === 'scissors' && computerSelection === 'rock')
   ) {
+    createScoreBoardComment(lose);
     keepScore(0, 1);
-    let li = document.createElement('li');
-    li.textContent = lose;
-    scoreList.prepend(li);
   }
 
   button.addEventListener('click', function () {
@@ -209,29 +135,98 @@ const game = (playerSelection, computerSelection) => {
   });
 };
 
-// Event listeners
-button.addEventListener('click', startGame);
-button.addEventListener('mouseover', addShadow);
-button.addEventListener('mouseleave', removeShadow);
+const keepScore = (player, com) => {
+  playerScore += player;
+  computerScore += com;
 
-tiles.forEach((tile) => tile.addEventListener('mouseover', addShadow));
-tiles.forEach((tile) => tile.addEventListener('mouseleave', removeShadow));
-comTiles.forEach((comTile) => comTile.addEventListener('mouseover', addShadow));
-comTiles.forEach((comTile) =>
-  comTile.addEventListener('mouseleave', removeShadow)
-);
+  youScore.textContent = playerScore;
+  comScore.textContent = computerScore;
+  button.textContent = click;
+
+  if (playerScore < 5 && computerScore < 5) {
+    button.addEventListener('click', function () {
+      comTile.style.boxShadow = 'none';
+      comTile.addEventListener('mouseover', addShadow);
+      comTile.addEventListener('mouseleave', removeShadow);
+      tiles.forEach((tile) => (tile.style.boxShadow = 'none'));
+      tiles.forEach((tile) => tile.addEventListener('mouseover', addShadow));
+      tiles.forEach((tile) =>
+        tile.addEventListener('mouseleave', removeShadow)
+      );
+      isGameOver = false;
+      startGame();
+    });
+  } else if (playerScore >= 5) {
+    button.textContent = winButton;
+    createScoreBoardComment(wonGame);
+    reset();
+  } else if (computerScore >= 5) {
+    button.textContent = loseButton;
+    createScoreBoardComment(lostGame);
+    reset();
+  }
+};
+
+const createScoreBoardComment = (result) => {
+  let li = document.createElement('li');
+  li.textContent = result;
+  scoreList.prepend(li);
+};
+
+const reset = () => {
+  youScore.textContent = playerScore;
+  comScore.textContent = computerScore;
+  playerScore = 0;
+  computerScore = 0;
+  playerSelection = null;
+  computerSelection = null;
+  choiceMade = false;
+  isGameOver = !isGameOver;
+  button.addEventListener('click', playAgain);
+}
+
+const playAgain = () => {
+  youScore.textContent = playerScore;
+  comScore.textContent = computerScore;
+
+  comTile.style.boxShadow = 'none';
+  comTile.addEventListener('mouseover', addShadow);
+  comTile.addEventListener('mouseleave', removeShadow);
+  tiles.forEach((tile) => (tile.style.boxShadow = 'none'));
+  tiles.forEach((tile) => tile.addEventListener('mouseover', addShadow));
+  tiles.forEach((tile) => tile.addEventListener('mouseleave', removeShadow));
+  startGame();
+};
+
+
+
+
+
+
+
+
+const initialLoad = () => {
+  button.addEventListener('click', startGame);
+  button.addEventListener('mouseover', addShadow);
+  button.addEventListener('mouseleave', removeShadow);
+
+  tiles.forEach((tile) => tile.addEventListener('mouseover', addShadow));
+  tiles.forEach((tile) => tile.addEventListener('mouseleave', removeShadow));
+  comTile.addEventListener('mouseover', addShadow);
+  comTile.addEventListener('mouseleave', removeShadow);
+}
 
 // JQuery
 
 function addShadow() {
-  console.log(this);
   let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
   let colorString = '1px 1px 5px 5px ' + color;
   $(this).css('box-shadow', colorString);
 }
 
 function removeShadow() {
-  console.log(this);
   let removed = 'none';
   $(this).css('box-shadow', removed);
 }
+
+initialLoad();
